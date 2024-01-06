3bea0788b4bcadea230d7333286ccfb73f3605c4
rylinsoto <rksoto@uwaterloo.ca>
2024-01-06 18:45:10 -0500

    Values now displayed on LCD

--- pulse_oximeter.ino
+++ pulse_oximeter.ino
@@ -1,18 +1,18 @@
 // Author: Rylin Soto
 // Date: November 8th 2023
-// Version: V0.1.0
+// Version: V1.1.0
 
 // LCD
-//  #include <LiquidCrystal.h>
-//  LiquidCrystal lcd(8, 7, 5, 4, 3, 2);
-//  #define SCREEN_WIDTH 16
-//  #define SCREEN_HEIGHT 2
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#define SCREEN_WIDTH 16
+#define SCREEN_HEIGHT 2
 
 // Definitions
-#define NUM_HB_AVERAGES 10 // Number of heart beat periods stored
-// #define NUM_R_CONST 10          //Number of R constants stored
+#define NUM_HB_AVERAGES 10   // Number of heart beat periods stored
 #define MAX_R_PERIOD_SIZE 80 // Number of absorbance averages to store
-#define RISE_THRESHOLD 3     // Number of averages that are increasing to be considered rising edge
+#define RISE_THRESHOLD 3     // Number of increasing IR absorbance averages to be considered a rising edge
 
 // Global variables
 int T = 20; // ms to read values from sensor, take average over 20ms to avoid 50hz noise caused by electric light
@@ -20,8 +20,8 @@ float SpO2;
 float HR;
 
 // Setup hardware
-const int redLED = 3;
-const int irLED = 4;
+const int redLED = 6;
+const int irLED = 7;
 const int photoDiode = A0;
 
 void setup()
@@ -43,28 +43,24 @@ void setup()
   Serial.println("Beginning Oxygen Saturation and heart rate measurments");
 
   // Initialize LCD
-  //  lcd.begin(SCREEN_WIDTH, SCREEN_HEIGHT);
-  //  lcd.setCursor(0, 0);
-  //  lcd.println("Insert Finger");
+  lcd.begin(SCREEN_WIDTH, SCREEN_HEIGHT);
+  lcd.setCursor(0, 0);
+  lcd.println("Insert Finger");
 }
 
-void error()
+void display_LCD()
 {
-  Serial.println("Could not get SpO2 and/or hr, trying again...");
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("BPM: " + String(HR));
+  lcd.setCursor(1, 8);
+  lcd.print("SpO2: " + String(SpO2) + "%");
 }
-
-// void display_LCD(float SpO2, float HR)
-// {
-//   lcd.setCursor(1,0);
-//   lcd.print("BPM: %.0f%" + String(HR));
-//   lcd.setCursor(1,8);
-//   lcd.print("SpO2: %.2f%", String(SpO2));
-// }
 
 void loop()
 {
-  // *************************** LED pattern variables ***************************
-  unsigned long startTime;
+  //*************************** LED pattern variables ***************************
+  uint32_t startTime;
   float absorbance;
   float irSum, irBuffer[MAX_R_PERIOD_SIZE], irLast;
   float redSum, redBuffer[MAX_R_PERIOD_SIZE], redLast;
@@ -80,7 +76,7 @@ void loop()
 
   int n = 0;
 
-  //*************************** SpO2 calc variables ***************************
+  //*************************** SpO2 variables ***************************
   int ptr = 0;
 
   float IRmax = 0;
@@ -90,13 +86,13 @@ void loop()
   float R = 0;
   int sampleCounter = 0;
 
-  //*************************** Heart rate variables ***************************
+  //*************************** HR variables ***************************
   float irBefore = 0;
   bool rising = false;
   int rise_count = 0;
-  unsigned long last_beat_ms;
+  uint32_t last_beat_ms;
 
-  int hbPeriods[NUM_HB_AVERAGES];
+  uint32_t hbPeriods[NUM_HB_AVERAGES];
   int m = 0;
   for (int i = 0; i < NUM_HB_AVERAGES; i++)
   {
@@ -151,9 +147,9 @@ void loop()
     {
       sampleCounter = 0;
       IRmax = 0;
-      IRmin = 1023;
+      IRmin = 1023; // 10 bit ADC resolution
       REDmax = 0;
-      REDmin = 1023; // 10 bit ADC resolution
+      REDmin = 1023;
 
       for (int i = 0; i < MAX_R_PERIOD_SIZE; i++)
       {
@@ -173,7 +169,7 @@ void loop()
       // R = (ac absorbance red /dc absorbance red) / (ac absorbance IR /dc absorbance IR)
       // DC absorbance cancles out (i.e. non pulsating arterial absorbance)
       R = ((REDmax - REDmin) / REDmin) / ((IRmax - IRmin) / IRmin);
-      Serial.println(String(R));
+      // Serial.println(String(R));
     }
 
     // *************************** Heart Rate Calculation ***************************
@@ -192,7 +188,7 @@ void loop()
         hbPeriods[m] = millis() - last_beat_ms;
         last_beat_ms = millis();
 
-        long unsigned avgPeriod = 0;
+        uint32_t avgPeriod = 0;
 
         // store the number of good measures (not floating more than 10%) in the last 10 peaks
         int numGoodReadings = 0;
@@ -209,25 +205,25 @@ void loop()
         m %= NUM_HB_AVERAGES;
 
         // *************************** Validating and Sending Values ***************************
-        // Record HR if there are at least 5 good measures
-        if (numGoodReadings > 4)
+        // Record HR if there are at least 3 good measures
+        if (numGoodReadings >= 3)
         {
-          HR = (avgPeriod / numGoodReadings) * 1000; // BPMS to BPM
-          SpO2 = 22.6 * R + 95.842;                  // Oxygen saturation curve
+          HR = (avgPeriod / numGoodReadings) / 60000; // BPMS to BPM
+          SpO2 = 22.6 * R + 95.842;                   // Oxygen saturation curve
 
-          // if((HR > 40 && HR <220) && (SpO2 > 70 && SpO2 <110))
-          // {
-          Serial.println(String(HR) + "bpm  " + String(SpO2) + "%");
-
-          // display_LCD();
-          // }
-          // else error();
+          if ((HR > 40 && HR < 220) && (SpO2 > 70 && SpO2 < 110))
+          {
+            // Serial.println(String(HR) + "bpm  " + String(SpO2) + "%");
+            display_LCD();
+          }
+          else
+            Serial.println("Error");
         }
-        else if (numGoodReadings <= 3)
+        else if (numGoodReadings < 3)
         {
-          Serial.println("Insert Finger");
-          //  lcd.setCursor(0, 0);
-          //  lcd.println("Insert Finger");
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.println("Insert Finger");
         }
       }
     }
